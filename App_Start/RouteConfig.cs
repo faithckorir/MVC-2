44f5d31ec68f7b0dc55cd6d498f1b6340a56e147
faithckorir <faithckorir@gmail.com>
2020-09-09 12:40:01 +0300

    tested out different actionresult methods

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -13,6 +13,13 @@ namespace trial2_mvc
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+
+            routes.MapRoute(
+            "Employee", "Employee/{name}", new
+                {
+                 controller = "Employee", action = "Search", name = UrlParameter.Optional
+                });
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace trial2_mvc.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public string GetAllCustomers()
+        {
+            return @"<ul>
+      <li>Ali Raza</li>
+      <li>Mark Upston</li>
+      <li>Allan Bommer</li>
+      <li>Greg Jerry</li>
+   </ul>";
+        }
+    }
+}
